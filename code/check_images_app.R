82ac6eb30c4cc2e837d37a75711590e05fa8eb66
Daniel Medina <danielmedinaneira555@gmail.com>
2021-02-19 21:38:59 -0500

    add the list of words to the app to check images

--- code/check_images_app.R
+++ code/check_images_app.R
@@ -1,26 +1,34 @@
 library(shiny)
 library(here)
 
+# obtain list of all the words (from the data set)
+words <- readLines(here("data/clean/renamed_duplicates.txt"))
 # obtain the paths for all the images (to render the images of the app)
 imgs <- list.files(here("data/media/"), full.names = TRUE)
 # create empty list to add bad images
 bad_imgs <- vector("list", length(imgs))
 
 ui <- fluidPage(
+  h3(textOutput("word")),
   imageOutput("image"),
-  # ADD THE WORDS, TO COMPARE AND DECIDE IF IT'S A GOOD OR A BAD IMAGE :)
   actionButton("button_good", "Next!"),
   actionButton("button_bad", "Bad image :("),
+  HTML("<br>"),
   actionButton("button_back", "Back")
 )
 
 server <- function(input, output, session) {
   # reactive element as a counter, it counts both buttons
   i <- reactive(input$button_good + input$button_bad - input$button_back)
+  
+  # render text
+  output$word <- renderText(words[i() + 1])
+  
   # render image
   output$image <- renderImage({
     list(src = imgs[i() + 1])
   }, deleteFile = FALSE)
+  
   # fill the list with the image shown in the screen at that moment
   observeEvent(input$button_bad, {
     bad_imgs[[i()]] <<- imgs[i()]
