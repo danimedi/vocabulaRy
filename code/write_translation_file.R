e272343d3b72838a0ab9d8f4943a19c7b011df97
Daniel Medina <danielmedinaneira555@gmail.com>
2021-02-28 13:41:25 -0500

    fix incorrect encoding of the data set when open with Excel

--- code/write_translation_file.R
+++
@@ -1,7 +0,0 @@
-library(readr)
-library(here)
-
-dat <- read_csv2(here("data/data_set.csv"))
-filt_dat <- dat[ , c("english", "spanish")]
-
-write_delim(filt_dat, here("data/translation_data.txt"), delim = "\t")
