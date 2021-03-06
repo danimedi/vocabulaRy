99e0ef366c2e188591e42edabdb4261d419b0a72
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-06 17:56:31 -0500

    add code to check the downloaded audio files

---
+++ code/check_downloaded_audios.R
@@ -0,0 +1,19 @@
+#' Check the audio files from a list of words
+#'
+#' @param words vector of strings containing the words
+#' @param audio_folder folder containing the audio files
+#'
+#' @return A list with 2 elemnts indicating the problems, if any, between the data set and
+#' the audio files.
+#' @export
+#'
+#' @examples
+
+check_downloaded_audios <- function(words, audio_folder) {
+  audios <- list.files(here::here(audio_folder), pattern = "[.]mp3$")
+  words <- paste0(words, ".mp3")
+  list(
+    words_without_audios = words[!words %in% audios],
+    audios_without_words = audios[!audios %in% words]
+  )
+}
