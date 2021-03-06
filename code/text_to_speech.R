e3964826007267b80303b590e82c520ce49c4786
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-06 16:36:16 -0500

    improve code of text to speech to avoid downloading files more than once and dealing with possible errors

--- code/text_to_speech.R
+++ code/text_to_speech.R
@@ -17,31 +17,36 @@
 text_to_speech <- function(
   words, 
   voice,
-  folder_path = choose.dir(caption = "Select folder where audio files will be saved"),
+  folder_path,
   sleep_time = 2
 ) {
   # API web page
   API <- "https://api.soundoftext.com/"
   # loop to obtain the audios and download them
   for (word in words) {
-    # sleep
-    Sys.sleep(abs(rnorm(1, sleep_time)))
-    # request the word and obtain the ID of the audio
-    req <- httr::POST(
-      url = API,
-      body = list(
-        "engine" = "Google", 
-        "data" = list("text" = word, "voice" = voice)
-      ),
-      path = "/sounds",
-      encode = "json",
-      config = list("Content-Type" = "application/json")
-    )
-    id <- httr::content(req)$id
-    # get the direction of the audio from the id in the response
-    res <- httr::GET(url = API, path = paste0("/sounds/", id))
-    url <- httr::content(res)$location
-    # download the audios
-    download.file(url, paste0(folder_path, "/", word, ".mp3"), mode = "wb")
+    file_name <- paste0(word, ".mp3")
+    if (!file_name %in% list.files(here::here(folder_path))) {
+      try({
+        # sleep
+        Sys.sleep(abs(rnorm(1, sleep_time)))
+        # request the word and obtain the ID of the audio
+        req <- httr::POST(
+          url = API,
+          body = list(
+            "engine" = "Google", 
+            "data" = list("text" = word, "voice" = voice)
+          ),
+          path = "/sounds",
+          encode = "json",
+          config = list("Content-Type" = "application/json")
+        )
+        id <- httr::content(req)$id
+        # get the direction of the audio from the id in the response
+        res <- httr::GET(url = API, path = paste0("/sounds/", id))
+        url <- httr::content(res)$location
+        # download the audios
+        download.file(url, here::here(folder_path, file_name), mode = "wb")
+      })
+    }
   }
 }
