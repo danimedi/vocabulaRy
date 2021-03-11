2f58e550580e0fd7d73365efe2bed27295111995
Daniel Medina <danielmedinaneira555@gmail.com>
2021-03-11 10:57:07 -0500

    create code and output including columns of different languages

--- code/generate_output.R
+++ code/generate_output.R
@@ -4,16 +4,24 @@
 #' @param in_dat main_data set containing all the languages
 #' @param out_dat data_set to be generated with the function
 #' @param language language to be filtered
+#' @param help_column string (or vector of strings) indicating the names of the additional
+#' columns to be included in the output data set
 #'
 #' @return Create a CSV file
 #' @export
 #'
 #' @examples
 #' 
-generate_output <- function(in_dat, out_dat, language) {
+generate_output <- function(in_dat, out_dat, language, help_language = c()) {
   # read input data and filter
   dat <- suppressMessages(readr::read_csv2(in_dat))
-  dat <- dplyr::select(dat, word, image_html, tidyselect::starts_with(language))
+  # include the help language column (optional)
+  if (length(help_language) > 0) {
+    dat <- dplyr::select(dat, word, image_html, tidyselect::starts_with(language), 
+                         tidyselect::all_of(help_language))
+  } else {
+    dat <- dplyr::select(dat, word, image_html, tidyselect::starts_with(language))
+  }
   # remove audio column (no html) if present
   i <- vapply(dat, function(x) !all(startsWith(x, "vocab-")), logical(1))
   dat <- dat[,i]
