e1cd5e196667251d2273d203c729faf68f5e01d4
Daniel Medina <danielmedinaneira555@gmail.com>
2021-05-12 08:21:38 -0500

    Improve handling of missing audios in the function to download audios

--- R/download_audios.R
+++ R/download_audios.R
@@ -5,15 +5,10 @@
 #'
 download_audios <- function(language, language_code, dir_media) {
   dat <- data_set()
-  res <- tts_forvo(dat$japanese, language_code, language, dir_media)
-
-  # Use the results to create a text file with the words that don't have audios
-  URLdecode2 <- function(URL) {
-    vapply(URL, function(x) {
-      x <- URLdecode(x)
-      Encoding(x) <- "UTF-8"
-      x
-    }, character(1), USE.NAMES = FALSE)
-  }
-  writeLines(URLdecode2(words), file.path(dir_media, "problems.txt"), sep = "\n\n", useBytes = TRUE)
+  tts_forvo(dat[[language]], language_code, language, dir_media)
+  # Recognize the file with the words with missing audios to add or remove the downloaded words.
+  # Compare the downloaded audios in the medial directory with the audios in the data set.
+  audio_data <- paste0("vocab-", language, "-", dat[[language]], ".mp3")
+  audio_files <- list.files(dir_media)
+  writeLines(setdiff(audio_data, audio_files), file.path(dir_media, "missing_audios.txt"), useBytes = TRUE)
 }
