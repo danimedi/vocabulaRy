006d3efc81809e73a451d74d20e616843f2c1fcc
Daniel Medina <danielmedinaneira555@gmail.com>
2021-05-12 10:34:36 -0500

    Specify the pattern to include only the audio (mp3) files

--- R/download_audios.R
+++ R/download_audios.R
@@ -11,7 +11,7 @@ download_audios <- function(language) {
   # Recognize the file with the words with missing audios to add or remove the downloaded words.
   # Compare the downloaded audios in the medial directory with the audios in the data set.
   audio_data <- paste0("vocab-", language, "-", dat[[language]], ".mp3")
-  audio_files <- list_files2(lookup$audio_path)
+  audio_files <- list_files2(lookup$audio_path, pattern = "[.]mp3$")
   missing <- setdiff(audio_data, audio_files)
   writeLines(missing, file.path(lookup$audio_path, "missing_audios.txt"), useBytes = TRUE)
   # Remove audios that are not present in the data set
