b493c72c8563bb8a8e680dd0377a10fe9fee8d7a
Daniel Medina <danielmedinaneira555@gmail.com>
2021-05-12 10:15:05 -0500

    Use the data for the language, language codes and the path, and remove unused audios

--- R/download_audios.R
+++ R/download_audios.R
@@ -3,12 +3,18 @@
 #' This functions is used to download audios and store them in the appropriate directory.
 #' It also creates a text file with the missing words (words without available audios).
 #'
-download_audios <- function(language, language_code, dir_media) {
+download_audios <- function(language) {
+  i <- audio_language_and_path()$language == language
+  lookup <- audio_language_and_path()[i, ]
   dat <- data_set()
-  tts_forvo(dat[[language]], language_code, language, dir_media)
+  tts_forvo(dat[[language]], lookup$language_code, lookup$language, lookup$audio_path)
   # Recognize the file with the words with missing audios to add or remove the downloaded words.
   # Compare the downloaded audios in the medial directory with the audios in the data set.
   audio_data <- paste0("vocab-", language, "-", dat[[language]], ".mp3")
-  audio_files <- list.files(dir_media)
-  writeLines(setdiff(audio_data, audio_files), file.path(dir_media, "missing_audios.txt"), useBytes = TRUE)
+  audio_files <- list.files(lookup$audio_path)
+  missing <- setdiff(audio_data, audio_files)
+  writeLines(missing, file.path(lookup$audio_path, "missing_audios.txt"), useBytes = TRUE)
+  # Remove audios that are not present in the data set
+  unused <- setdiff(audio_files, audio_data)
+  file.remove(file.path(lookup$audio_path, unused))
 }
